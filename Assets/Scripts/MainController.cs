d000cbb3f3606e0c63bcd395cc1ff422515d3978
Altynai <772773671@qq.com>
2018-01-22 19:42:53 +0800

    完成Oculus和Tv两个视角的场景建立

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour {
+
+	private bool startcombine = true;
+	private bool stopcombine = false;
+	private bool recovery = false;
+	private bool showcompoents = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, 1, 0);
+		if (startcombine) {
+			StartCoroutine ("stopCombine");
+		}
+		if (stopcombine){
+			StartCoroutine("Recovery");
+		}
+		if (recovery) {
+			StartCoroutine ("showCompoents");
+		}
+		if (showcompoents){
+			StartCoroutine("startCombine");
+		}
+	}
+
+	// 完成模型旋转
+	IEnumerator stopCombine(){
+		startcombine = false;
+		yield return new WaitForSeconds (1);
+		foreach (Transform child in transform) {
+			if (child.gameObject.name == "rabbit") {
+				child.gameObject.SetActive (true);
+				continue;
+			}
+			child.gameObject.GetComponent<Move> ().Stop();
+			child.gameObject.SetActive (false);
+		}
+		stopcombine = true;
+	}
+
+	//各个部分爆炸效果
+	IEnumerator Recovery(){
+		stopcombine = false;
+		yield return new WaitForSeconds (3);
+		foreach (Transform child in transform) {
+			if (child.gameObject.name == "rabbit") {
+				child.gameObject.SetActive (false);
+				continue;
+			}
+			child.gameObject.GetComponent<Move> ().Recovery();
+			child.gameObject.SetActive (true);
+		}
+		recovery = true;
+	}
+
+	//各个部分静止旋转效果
+	IEnumerator showCompoents(){
+		recovery = false;
+		yield return new WaitForSeconds (1);
+		foreach (Transform child in transform) {
+			if (child.gameObject.name == "rabbit") {
+				continue;
+			}
+			child.gameObject.GetComponent<Move> ().Stop();
+		}
+		showcompoents = true;
+	}
+	//各个部分合并旋转效果
+	IEnumerator startCombine(){
+		showcompoents = false;
+		yield return new WaitForSeconds (1);
+		foreach (Transform child in transform) {
+			if (child.gameObject.name == "rabbit") {
+				continue;
+			}
+			child.gameObject.GetComponent<Move> ().Restart();
+		}
+		startcombine = true;
+	}
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+	public float x = 0f;
+	public float y = 0f;
+	public float z = 0f;
+	private Vector3 direction;
+	private float speed;
+	private float origin;
+	// Use this for initialization
+	void Start () {
+		if (x != 0) {
+			direction = Vector3.right;
+			speed = x;
+		}
+		if (y != 0) {
+			direction = Vector3.up;
+			speed = y;
+		}
+		if (z != 0) {
+			direction = Vector3.forward;
+			speed = z;
+		}
+		origin = speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.name == "left") {
+			Debug.Log ("set speed " + speed);
+		}
+
+		transform.Translate (direction*speed*Time.deltaTime);
+	}
+
+	public void Stop(){
+		speed = 0;
+	}
+	public void Recovery(){
+		speed = -origin;
+	}
+	public void Restart(){
+		speed = origin;
+	}
+}
